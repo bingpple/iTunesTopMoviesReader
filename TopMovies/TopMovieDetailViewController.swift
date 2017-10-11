1dad613bd05513c9541e6ee3835d5eff49f4bb80
bingpple <bingpple@gmail.com>
2017-10-11 16:25:35 +1300

    Add support to player movie trailer

--- TopMovies/TopMovieDetailViewController.swift
+++ TopMovies/TopMovieDetailViewController.swift
@@ -1,5 +1,7 @@
 //
-//  TopMovieDetailViewController.swift
+//  TopMovieTableViewController.swift
+//
+//  The MIT License (MIT)
 //
 //  Copyright (c) 2017 Bing Cai
 //
@@ -23,20 +25,148 @@
 //
 
 import UIKit
-import WebKit
-import AVKit
+import FeedKit
 
-class TopMovieDetailViewController: UIViewController {
+let feedURL = URL(string: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/xml")!
+
+class TopMovieTableViewCell: UITableViewCell{
   
-  var movieDetailContent = ""
-  var movieName = ""
+  @IBOutlet weak var movieImageView: UIImageView!
+  @IBOutlet weak var movieNameLabel: UILabel!
+  @IBOutlet weak var moviePriceLabel: UILabel!
+  @IBOutlet weak var movieCategoryLabel: UILabel!
+  
+}
+
+class TopMovieTableViewController: UIViewController {
     
-  @IBOutlet weak var movieDetailWebView: WKWebView!
+  var feed: AtomFeed?
+  let parser = FeedParser(URL: feedURL)!
+  var movieDetailContent: String?
+  var movieName: String?
+    
+  @IBOutlet
+  var tableView: UITableView!
+   
+  lazy var refreshControl: UIRefreshControl = {
+    
+    let refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: #selector(TopMovieTableViewController.handleRefresh(_:)), for: UIControlEvents.valueChanged)
+    refreshControl.tintColor = .blue
+    refreshControl.attributedTitle =  NSAttributedString(string: "Refreshing Top Movies List")
+    return refreshControl
+    
+  }()
     
   override func viewDidLoad() {
+    
     super.viewDidLoad()
-    title = movieName
-    movieDetailWebView.loadHTMLString(movieDetailContent, baseURL: nil)
+        
+    if #available(iOS 10.0, *) {
+      tableView.refreshControl = refreshControl
+    } else {
+      tableView.addSubview(refreshControl)
+    }
+        
+    // parse the feed in async way
+    parseFeed()
+    
+  }
+
+  func parseFeed(){
+    
+    parser.parseAsync { [weak self] (result) in
+      self?.feed = result.atomFeed
+            
+    // Then back to the Main thread to update the UI.
+    DispatchQueue.main.async {
+      self?.title = self?.feed?.title
+      self?.tableView.reloadData()
+    }
+      
+    }
+    
+  }
+
+  @objc private func handleRefresh(_ refreshControl: UIRefreshControl) {
+    
+    // Simply retrive the feed again
+    parseFeed()
+        
+    // Refresh the table view
+    tableView.reloadData()
+    refreshControl.endRefreshing()
+    
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if (segue.identifier == "movieDetailSegue"){
+      let topMovieDetailViewController = segue.destination as! TopMovieDetailViewController
+      topMovieDetailViewController.movieDetailContent = (feed?.entries?[(tableView.indexPathForSelectedRow?.row)!].content?.value)!
+      topMovieDetailViewController.movieName = (feed?.entries?[(tableView.indexPathForSelectedRow?.row)!].title!)!
+      topMovieDetailViewController.movieTrailerAddress = (feed?.entries?[(tableView.indexPathForSelectedRow?.row)!].links![1].attributes?.href)!
+    }
+    
+  }
+  
+}
+
+//MARK: - UITableViewDataSource
+extension TopMovieTableViewController:UITableViewDataSource{
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+    
+  }
+    
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return feed?.entries?.count ?? 0
+    
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell") as! TopMovieTableViewCell
+    cell.movieCategoryLabel.text = feed?.entries?[indexPath.row].categories?[0].attributes?.label
+    cell.movieNameLabel.text = feed?.entries?[indexPath.row].title
+    let htmlContent = feed?.entries?[indexPath.row].content?.value
+    let pricePat = "\\$ *(\\d+(\\.\\d+)?)"
+    let matchedPrice = matches(for: pricePat, in: htmlContent!)
+    //Match movie price
+    cell.moviePriceLabel.text = matchedPrice[0]
+    let postImgPat = "<img.*?src=\"([^\"]*)\""
+    let matchedImgs = matches(for: postImgPat, in: htmlContent!)
+    //Match movie image element
+    let postImageElement = matchedImgs[0]
+    let postPicPat = "(http[^\\s]+(jpg|jpeg|png|tiff)\\b)"
+    let postPicUrl = matches(for: postPicPat, in: postImageElement)
+    //Match movie image url
+    let imageUrl:URL = URL(string: postPicUrl[0])!
+    DispatchQueue.global(qos: .userInitiated).async {
+      let imageData:NSData = NSData(contentsOf: imageUrl)!
+      // When from background thread, UI needs to be updated on main_queue
+      DispatchQueue.main.async {
+        let image = UIImage(data: imageData as Data)
+          cell.movieImageView.image = image
+        
+      }
+      
+    }
+    
+    return cell
+    
+  }
+  
+ func matches(for regex: String, in text: String) -> [String] {
+    
+    do {
+      let regex = try NSRegularExpression(pattern: regex)
+      let results = regex.matches(in: text,
+                                  range: NSRange(text.startIndex..., in: text))
+      return results.map {String(text[Range($0.range, in: text)!])}
+    } catch let error {
+      print("invalid regex: \(error.localizedDescription)")
+      return []
+    }
     
   }
   
